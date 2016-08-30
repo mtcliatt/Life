e77356db767c580e43668614c269b25f8b2b31f1
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-08-30 11:55:45 -0400

    code formatting

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-	if (process.platform != 'darwin')
+app.on('window-all-closed', () => {
+
+	if (process.platform != 'darwin') {
 		app.quit();
+	}
+
 });
 
-app.on('ready', function() {
+app.on('ready', () => {
+
 	mainWindow = new BrowserWindow({
+
 		width: 1000,
 		height: 800,
 		minHeight: 800,
 		minWidth: 1000,
 		frame: true,
+
 	});
 
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-	mainWindow.on('closed', function() {
+	mainWindow.on('closed', () => {
+
 		mainWindow = null;
+
 	});
+
 });
